584f835805d4b186ce4e562154351ac739bd39e6
pmilan1 <milan.parmar.0429@gmail.com>
2024-02-20 16:34:13 -0500

    Added "Add" course button functionality

--- app/src/main/java/com/example/anonymousgradingapp/MainActivity.java
+++ app/src/main/java/com/example/anonymousgradingapp/MainActivity.java
@@ -5,14 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button testButton;
+    private List<String> coursesList;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coursesList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coursesList);
+
+        Spinner spinnerCourses = findViewById(R.id.courseSpinner);
+        spinnerCourses.setAdapter(adapter);
+
+        EditText editTextCourseName = findViewById(R.id.addCourseEditText);
+        Button buttonAddCourse = findViewById(R.id.addCourseButton);
+
+        buttonAddCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String courseName = editTextCourseName.getText().toString();
+                if (!courseName.isEmpty()) {
+                    coursesList.add(courseName);
+                    adapter.notifyDataSetChanged();
+                    editTextCourseName.setText("");
+                }
+            }
+        });
     }
 }
