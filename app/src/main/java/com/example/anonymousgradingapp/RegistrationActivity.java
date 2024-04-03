d9462d5b0ca198040e9d711ceb7e3dd522d23ee4
Deven Parmar <95242927+deven6@users.noreply.github.com>
2024-04-03 14:51:20 -0400

    spelling correction

--- app/src/main/java/com/example/anonymousgradingapp/RegistrationActivity.java
+++ app/src/main/java/com/example/anonymousgradingapp/RegistrationActivity.java
@@ -14,7 +14,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registeration);
+        setContentView(R.layout.activity_registration);
 
         registerButton = (Button) findViewById(R.id.registerButton);
         cancelButton = (Button) findViewById(R.id.cancelButton);
