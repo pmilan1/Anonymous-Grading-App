d1f5c722798056eb1178dd1ee1c4ea7fce326781
Deven Parmar <95242927+deven6@users.noreply.github.com>
2024-04-18 15:25:13 -0400

    Implementing CSV file I/O

--- app/src/main/java/com/example/anonymousgradingapp/MainActivity.java
+++ app/src/main/java/com/example/anonymousgradingapp/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.anonymousgradingapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -22,10 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String PREF_NAME = "MyPrefs";
     public static final String COURSES_KEY = "courses";
+    private static final int PICK_CSV_FILE = 1;
 
-    public List<String> coursesList;
+    public List<String> coursesList, rosterList;
     private ArrayAdapter<String> adapter;
-    private Button buttonExams, barcodeMap, addCourse;
+    private Button buttonExams, barcodeMap, addCourse, buttonUpload;
     private EditText courseName;
     private Spinner spinnerCourses;
 
@@ -40,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
         coursesList = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coursesList);
+        rosterList = new ArrayList<>();
 
         spinnerCourses = (Spinner) findViewById(R.id.courseSpinner);
         spinnerCourses.setAdapter(adapter);
@@ -47,7 +70,10 @@ public class MainActivity extends AppCompatActivity {
         courseName = (EditText) findViewById(R.id.courseNameEditText);
         addCourse = (Button) findViewById(R.id.addCourseButton);
         barcodeMap = (Button) findViewById(R.id.barcodeMapButton);
-        buttonExams = findViewById(R.id.buttonExams);
+        buttonExams = (Button) findViewById(R.id.buttonExams);
+        buttonUpload = (Button) findViewById(R.id.uploadButton);
+        requestForStoragePermissions();
+
 
         addCourse.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
@@ -79,6 +105,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        spinnerCourses.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                buttonUpload.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openFile();
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         /*
         private void loadSpinnerData() {
             DatabaseManager db = new DatabaseManager(getApplicationContext());
@@ -101,5 +145,83 @@ public class MainActivity extends AppCompatActivity {
             coursesList.addAll(set);
             adapter.notifyDataSetChanged();
         }
+    }
+    private static final int STORAGE_PERMISSION_CODE = 23;
+
+    private void requestForStoragePermissions() {
+        //Android is 11 (R) or above
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            try {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                storageActivityResultLauncher.launch(intent);
+            }catch (Exception e){
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                storageActivityResultLauncher.launch(intent);
+            }
+        }
+
+    }
+    private ActivityResultLauncher<Intent> storageActivityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>(){
+
+                        @Override
+                        public void onActivityResult(ActivityResult o) {
+                            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                                //Android is 11 (R) or above
+                                if(Environment.isExternalStorageManager()){
+                                    //Manage External Storage Permissions Granted
+                                    //Log.d(TAG, "onActivityResult: Manage External Storage Permissions Granted");
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Storage Permissions Denied", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                //Below android 11
+
+                            }
+                        }
+                    });
+    private void openFile(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/csv"); //MIME type for csv file
+        startActivityForResult(intent, PICK_CSV_FILE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_CSV_FILE && resultCode == Activity.RESULT_OK){
+            Uri uri = null;
+            if(data != null){
+                uri = data.getData(); //Gets file location from intent
+                if(uri != null){
+                    getCSVFromUri(uri); //Read the csv
+                }
+
+            }
+        }
+    }
+    private void getCSVFromUri(Uri uri){
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            if(inputStream != null){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                line = reader.readLine(); //Skips the header line
+                while ((line = reader.readLine()) != null){
+                    rosterList.add(line);
+                }
+                inputStream.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(this, "CSV file read error", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
