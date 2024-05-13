775be27ad2975e2a50771150dd2f88c4a3ee9156
pmilan1 <milan.parmar.0429@gmail.com>
2024-05-12 22:16:14 -0400

    Fixed sign in issue (did not sign out). Removed unused buttons.

--- app/src/main/java/com/example/anonymousgradingapp/LoginActivity.java
+++ app/src/main/java/com/example/anonymousgradingapp/LoginActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.amplifyframework.auth.cognito.result.AWSCognitoAuthSignOutResult;
 import com.amplifyframework.core.Amplify;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username, password;
-    private Button signupButton, forgotPasswordButton, loginButton, bypassButton;
+    private Button signupButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +27,54 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
         signupButton = (Button) findViewById(R.id.registerButton);
-        forgotPasswordButton = (Button) findViewById(R.id.forgotButton);
         loginButton = (Button) findViewById(R.id.loginButton);
-        bypassButton = findViewById(R.id.bypassButton);
+
+        Amplify.Auth.signOut(
+                signOutResult -> {
+                    if (signOutResult instanceof AWSCognitoAuthSignOutResult.CompleteSignOut) {
+                        Log.i("AmplifyRegister", "Sign out successful");
+                    }
+                    else {
+                        Log.e("AmplifyRegister", "Sign out failed");
+                    }
+                }
+        );
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Amplify.Auth.signIn(username.getText().toString(),
+                Amplify.Auth.signIn(
+                        username.getText().toString(),
                         password.getText().toString(),
-                        result -> Log.i("AmplifyRegister", "Result: " + result.toString()),
-                        error -> Log.e("AmplifyRegister", "Error: " + error.toString()));
-                Amplify.Auth.fetchAuthSession(
-                        result -> Log.i("AmplifyRegister", "Result: " + result.toString()),
-                        error -> Log.e("AmplifyRegister", "Error: " + error.toString())
-                );
-                // add logic to login only if successful
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+
+                        result -> {
+                            Log.i("AmplifyRegister", "Result: " + result.toString());
+
+                            Amplify.Auth.fetchAuthSession(
+                                    authSessionResult -> {
+                                        Log.i("AmplifyRegister", "Result: " + authSessionResult.toString());
+
+                                        if (authSessionResult.isSignedIn()) {
+                                            Looper.prepare();
+                                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                            startActivity(i);
+                                        }
+                                        else {
+                                            Toast.makeText(LoginActivity.this, "Incorrect Login", Toast.LENGTH_SHORT).show();
+                                        }
+                                    },
+                                    authSessionError -> {
+                                        Log.e("AmplifyRegister", "Error " + authSessionError.toString());
+                                        Toast.makeText(getApplicationContext(), "Error: " + authSessionError.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                            );
+                        },
+                        error -> {
+                            Log.e("AmplifyRegister", "Error: " + error.toString());
+                            Looper.prepare();
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        });
             }
         });
 
@@ -49,22 +82,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {   // opens Registration page
                 Intent i = new Intent(getApplicationContext(), RegistrationActivity.class);
-                startActivity(i);
-            }
-        });
-
-        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
-                startActivity(i);
-            }
-        });
-
-        bypassButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
             }
         });
