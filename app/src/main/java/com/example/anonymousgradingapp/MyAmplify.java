a70b2940a9c5e6b6786082bf1cf2dbbc6246713f
pmilan1 <milan.parmar.0429@gmail.com>
2024-05-14 23:51:58 -0400

    Added test function for courses from DB

--- app/src/main/java/com/example/anonymousgradingapp/MyAmplify.java
+++ app/src/main/java/com/example/anonymousgradingapp/MyAmplify.java
@@ -6,8 +6,10 @@ import android.widget.Toast;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Course;
 
 public class MyAmplify extends Application {
     @Override
@@ -24,5 +26,21 @@ public class MyAmplify extends Application {
             e.printStackTrace();
             Log.e("AmplifyRegister", "Could not initialize Amplify", e);
         }
+        getCourses();
+    }
+    private void getCourses() {
+        Amplify.API.query(
+                ModelQuery.list(Course.class),
+                response -> {
+                    if (response.hasData()) {
+                        for (Course course : response.getData()) {
+                            Log.i("getCourses", "Course Name: " + course.getCoursename());
+                        }
+                    } else {
+                        Log.e("getCourses", "No data retrieved");
+                    }
+                },
+                error -> Log.e("getCourses", "Query failed", error)
+        );
     }
 }
